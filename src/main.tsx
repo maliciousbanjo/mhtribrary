bf949fab59b1ca793430255189a096f9ac8d64ad
MaliciousBanjo <maliciousbanjo@gmail.com>
2024-10-13 10:51:40 -0500

    Incorporate mh3-data 0.9.4 & hash routing
    - closes #9

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './main.scss';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { RouterProvider, createHashRouter } from 'react-router-dom';
 import { DamagePage } from './routes/damage/damage-page.tsx';
 import { RootLayout } from './routes/root-layout';
 import { AboutPage } from './routes/about/about-page.tsx';
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     element: <RootLayout />,
     children: [
