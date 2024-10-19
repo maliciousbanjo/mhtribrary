ef18f788e8cf8f8bbbd265d703c34f88e40d8ff6
MaliciousBanjo <maliciousbanjo@gmail.com>
2024-10-18 20:52:15 -0500

    Added react keys

--- src/routes/damage/weapon/weapon-info/sharpness-graph.tsx
+++ src/routes/damage/weapon/weapon-info/sharpness-graph.tsx
@@ -1,3 +1,5 @@
+import { v3 as uuidv3 } from 'uuid';
+
 interface SharpnessGraphProps {
   sharpness: number[];
 }
@@ -10,6 +12,7 @@ export function SharpnessGraph({ sharpness }: SharpnessGraphProps) {
     <div className="weapon-info__sharpness">
       {sharpness.map((sharpnessLength, index) => (
         <div
+          key={uuidv3(JSON.stringify(sharpness) + index, uuidv3.URL)}
           className={`sharpness-cell sharp${index}`}
           style={{
             width: `${sharpnessLength * 4}px`
