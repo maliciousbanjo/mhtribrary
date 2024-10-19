a3c161ef1bc56835214db528ae6b46fca10ea88e
MaliciousBanjo <maliciousbanjo@gmail.com>
2024-10-18 20:30:22 -0500

    Formatting for weapon info
    - Reduced harshness of the red in dark theme

--- src/routes/damage/weapon/weapon-info/weapon-info.tsx
+++ src/routes/damage/weapon/weapon-info/weapon-info.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
-import { Weapons, WeaponTypes } from 'mh3-data';
-import { SharpnessGraph } from './sharpness-graph';
 import { Icon } from '@blueprintjs/core';
-import { capitalize } from '../../../../utils/format-utils';
+import { Weapons, WeaponTypes } from 'mh3-data';
+import React from 'react';
+import { SharpnessGraph } from './sharpness-graph';
 
 interface WeaponInfoProps {
   weapon: WeaponTypes.Weapon<Weapons.WeaponClass>;
@@ -18,6 +17,24 @@ export function WeaponInfo({ weapon }: WeaponInfoProps) {
     awaken
   } = weapon;
 
+  const elementLabel = (
+    <>
+      <span>{secondaryAttack} </span>
+      <span className="element-type">{secondaryDamageType}</span>
+    </>
+  );
+
+  const slotsElement = React.useMemo<JSX.Element | JSX.Element[]>(() => {
+    const result: JSX.Element[] = [];
+    for (let i = 0; i < slots; i++) {
+      result.push(
+        <Icon tagName={'span'} className="slot-icon" icon="circle" size={13} />
+      );
+    }
+
+    return result.length > 0 ? result : <>---</>;
+  }, [slots]);
+
   return (
     <div className="weapon-info">
       <div>
@@ -29,16 +46,14 @@ export function WeaponInfo({ weapon }: WeaponInfoProps) {
           <label>Attack:</label> {attack}
         </div>
         <div>
-          {/* // TODO: Need to include awaken */}
-          <label>Element:</label> {secondaryAttack}{' '}
-          {capitalize(secondaryDamageType)}
+          <label>Element:</label>{' '}
+          {!awaken ? elementLabel : <>({elementLabel})</>}
         </div>
         <div>
           <label>Aff:</label> {affinity}%
         </div>
-        <div>
-          {/* // TODO: Use circle Icon for slots */}
-          <label>Slots:</label> {slots}
+        <div className="slots">
+          <label>Slots:</label> <>{slotsElement}</>
         </div>
       </div>
     </div>
